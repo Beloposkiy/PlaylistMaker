d527a722ad1f645a9fe893e610eeb481cec5b736
George Belopolsky <georgy.belopolsky@yandex.ru>
2025-04-18 21:42:32 +0300

    first home work

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val searchButton = findViewById<MaterialButton>(R.id.search_button)
+        val mediaLibraryButton = findViewById<MaterialButton>(R.id.media_library_button)
+        val settingsButton = findViewById<MaterialButton>(R.id.settings_button)
+
+        searchButton.setOnClickListener{
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediaLibraryButton.setOnClickListener{
+            val mediaLibraryIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(mediaLibraryIntent)
+        }
+
+        settingsButton.setOnClickListener{
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
