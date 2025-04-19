52cc7cacaae9244092555e3a4235ee88b6426cd3
George Belopolsky <georgy.belopolsky@yandex.ru>
2025-04-19 04:20:20 +0300

    first home work

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -9,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
 
 class SettingsActivity : AppCompatActivity() {
@@ -16,36 +18,11 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val shareButton = findViewById<View>(R.id.share_button)
-        val supportButton = findViewById<View>(R.id.support_button)
-        val agreementButton= findViewById<View>(R.id.agreement_button)
-
-        val searchTabBarButton = findViewById<View>(R.id.search_tab_bar_button)
-        val mediaLibraryTabBarButton = findViewById<View>(R.id.media_library_tab_bar_button)
-        val settingsTabBarButton= findViewById<View>(R.id.settings_tab_bar_button)
-
-
-
-        shareButton.findViewById<TextView>(R.id.label).setText(R.string.share)
-        shareButton.findViewById<ImageView>(R.id.icon).setImageResource(R.drawable.ic_share)
-
-        supportButton.findViewById<TextView>(R.id.label).setText(R.string.support)
-        supportButton.findViewById<ImageView>(R.id.icon).setImageResource(R.drawable.ic_support)
-
-        agreementButton.findViewById<TextView>(R.id.label).setText(R.string.user_agreement)
-        agreementButton.findViewById<ImageView>(R.id.icon).setImageResource(R.drawable.ic_arrow_forward)
-
-        searchTabBarButton.findViewById<TextView>(R.id.label).setText(R.string.search)
-        searchTabBarButton.findViewById<ImageView>(R.id.icon).setImageResource(R.drawable.ic_search)
-
-        mediaLibraryTabBarButton.findViewById<TextView>(R.id.label).setText(R.string.mediaLibrary)
-        mediaLibraryTabBarButton.findViewById<ImageView>(R.id.icon).setImageResource(R.drawable.ic_media_library)
-
-        settingsTabBarButton.findViewById<TextView>(R.id.label).setText(R.string.settings)
-        settingsTabBarButton.findViewById<ImageView>(R.id.icon).setImageResource(R.drawable.ic_settings)
-
-
-
+        val toolbar = findViewById<MaterialToolbar>(R.id.settings_toolbar_button)
+        toolbar.setNavigationOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
 
     }
 }
