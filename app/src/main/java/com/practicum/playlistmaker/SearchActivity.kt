deaec7438bf448f45a94c7c302f60a2744f3e9a7
George Belopolsky <georgy.belopolsky@yandex.ru>
2025-05-06 11:46:25 +0300

    edit text

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,20 +1,46 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_search)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val toolbarNaviagationButton = findViewById<MaterialToolbar>(R.id.search_toolbar_button)
+        toolbarNaviagationButton.setNavigationOnClickListener {
+            finish()
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+
         }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,22 +1,30 @@
 package com.practicum.playlistmaker
 
-import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.button.MaterialButton
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
 
         val shareTextView = findViewById<MaterialTextView>(R.id.share_textView)
         val supportTextView = findViewById<MaterialTextView>(R.id.support_textView)
@@ -25,6 +33,16 @@ class SettingsActivity : AppCompatActivity() {
         val supportEmail = getString(R.string.email)
         val supportEmailSubject = Uri.encode(getString(R.string.support_subject))
         val supportEmailText = Uri.encode(getString(R.string.support_text))
+
+        val themeSwitcher = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        themeSwitcher.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
 
 
         shareTextView.setOnClickListener {
@@ -38,34 +56,31 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(Intent.createChooser(shareIntent, "Поделиться приложением через"))
         }
 
-        supportTextView.setOnClickListener{
-            val uri = Uri.parse("mailto:$supportEmail?subject=$supportEmailSubject&body=$supportEmailText")
+        supportTextView.setOnClickListener {
+            val uri =
+                "mailto:$supportEmail?subject=$supportEmailSubject&body=$supportEmailText".toUri()
 
             val supportIntent = Intent(Intent.ACTION_SENDTO, uri)
 
             try {
                 startActivity(Intent.createChooser(supportIntent, "Выберете почтовое прилолжение"))
-            } catch(e: ActivityNotFoundException){
-                Toast.makeText(this, "Нет устоновленного почтового клиента", Toast.LENGTH_LONG).show()
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "Нет устоновленного почтового клиента", Toast.LENGTH_LONG)
+                    .show()
             }
         }
 
         agreementTextView.setOnClickListener {
             val url = getString(R.string.agreement_link)
 
-            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            val agreementIntent = Intent(Intent.ACTION_VIEW, url.toUri())
 
             startActivity(agreementIntent)
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_settings)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val toolbar = findViewById<MaterialToolbar>(R.id.settings_toolbar_button)
-        toolbar.setNavigationOnClickListener {
+        val toolbarNaviagationButton = findViewById<MaterialToolbar>(R.id.settings_toolbar_button)
+        toolbarNaviagationButton.setNavigationOnClickListener {
             finish()
         }
     }
